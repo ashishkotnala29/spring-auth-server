e4295ce327b925d5444f5227ae7ed4cf84fabc4c
Ashish Kotnala <ashishkotnala1.ak@gmail.com>
2020-03-22 20:48:48 +0530

    initial commit

---
+++ src/test/java/com/ashish/authserver/SpringAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
