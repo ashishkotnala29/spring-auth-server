1791a8053693e313ebaa467540388f5435bd2975
Ashish Kotnala <ashishkotnala1.ak@gmail.com>
2020-03-25 01:27:08 +0530

    OAuthServer: jdbc auth and tokenStore changes

--- src/main/java/com/ashish/authserver/config/AuthServerConfiguration.java
+++ src/main/java/com/ashish/authserver/config/AuthServerConfiguration.java
@@ -4,41 +4,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
-public class AuthServerConfiguration extends WebSecurityConfigurerAdapter implements AuthorizationServerConfigurer {
-
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+public class AuthServerConfiguration implements AuthorizationServerConfigurer {
 
     @Autowired
-    AuthenticationManager authenticationManager;
+    private PasswordEncoder passwordEncoder;
 
-    PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Bean
+    TokenStore jdbcTokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.checkTokenAccess("permitAll()");
+        security.checkTokenAccess("isAuthenticated()").tokenKeyAccess("permitAll()");
     }
 
     @Override
-    public void configure(ClientDetailsServiceConfigurer client) throws Exception {
-        client.inMemory().withClient("web").secret(passwordEncoder.encode("webpass")).scopes("READ","WRITE")
-                .authorizedGrantTypes("refresh_token","authorization_code","password","client_credentials")
-                .accessTokenValiditySeconds(3600);
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
     }
 
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoint) throws Exception {
-        endpoint.authenticationManager(authenticationManager);
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(jdbcTokenStore());
+        endpoints.authenticationManager(authenticationManager);
     }
 }
---
+++ src/main/java/com/ashish/authserver/service/MyUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.ashish.authserver.service;
+
+import com.ashish.authserver.model.AuthUserDetail;
+import com.ashish.authserver.model.User;
+import com.ashish.authserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AccountStatusUserDetailsChecker;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepo.findByUsername(username);
+        optionalUser.orElseThrow(()-> new UsernameNotFoundException("Username/Password is wrong!"));
+        UserDetails userDetails = new AuthUserDetail(optionalUser.get());
+        new AccountStatusUserDetailsChecker().check(userDetails);
+        return userDetails;
+    }
+}
